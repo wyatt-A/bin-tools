e1b3a7977e60474c7a2196cbd292c4c7fd93408c
wyatt-a <wyatt-a@users.noreply.github.com>
2024-04-24 17:15:26 -0400

    update

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,71 @@
-use std::{collections::HashMap, error::Error, iter::Sum, mem::zeroed, ops::{Add, AddAssign, Deref}, time::Instant};
+use std::{collections::HashMap, error::Error, fs::File, io::Read, iter::Sum, mem::zeroed, ops::{Add, AddAssign, Deref}, time::Instant};
 use rand::{thread_rng, Rng};
-use num_traits::{real::Real, Num, Zero};
+use num_traits::{real::Real, Float, Num, Zero};
+use sheet_calc::SpreadSheet2D;
+use tiff::encoder::{colortype::{CMYK32Float, ColorType, Gray32Float, Gray64Float}, TiffEncoder};
+use rayon::prelude::*;
 
+fn main() -> Result<(),Box<dyn Error>> {
 
-fn main() {
-    println!("Hello, world!");
+    let now = Instant::now();
 
-
-    // traverse set of coordinates,
-    // recording min and max as we go ...
-
-    let mut rng = thread_rng();
-
+    let mut f = File::open("/Users/Wyatt/qpath_test_data/1125ganglio_slide1_detection-measurements.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
     
+    println!("parsing spreadsheet ...");
+    let s = SpreadSheet2D::from_string(s, "\t", 0);
 
-    let n = 30_000;
-    let x:Vec<f32> = (0..n).map(|_| rng.gen_range(-1000. .. 1000.) ).collect();
-    let y:Vec<f32> = (0..n).map(|_| rng.gen_range(-1000. .. 1000.) ).collect();
-    let vals:Vec<f32> = (0..n).map(|_| rng.gen_range(1. .. 2.) ).collect();
+    println!("extracting columns ...");
+    let x = s.exract_column("Centroid X")?;
+    let y = s.exract_column("Centroid Y")?;
 
-
-    let m = mean(&x);
-
-
-    let n_elems = x.len();
-
-    let bin = Bin2D::new(&x,&y,&vals,1.);
+    let stats = [Statistic::Mean];
 
 
+    for (header,vals) in s.column_headers().iter().zip(s.columns_numeric()) {
+
+        println!("binning feature ...");
+        let bin = Bin2D::new(&x,&y,&vals,100.);
+    
+        let dims = bin.dimensions();
+        let width = dims.0;
+        let height = dims.1;
+    
+        for stat in stats {
+            let fname = format!("{:?}",stat);
+            println!("writing {} ...",fname);
+            let agg = bin.aggregate(stat);
+            let file = File::create(fname + "_" + header + ".tiff").expect("Failed to create file");
+            // Create a TIFF encoder
+            let mut tiff_encoder = TiffEncoder::new(file).expect("Failed to create TIFF encoder");
+            // Write the image data to the TIFF file
+            tiff_encoder
+                .write_image::<Gray64Float>(width as u32, height as u32,&agg)
+                .expect("Failed to write TIFF image");
+        }
+    }
+
+
+    // s.column_headers().par_iter().for_each(|header|{
+
+
+    // });
+
+    let dur = now.elapsed().as_millis();
+    println!("took {} ms",dur);
+    Ok(())
 }
 
 
 
 pub struct Bin2D<T> {
-    storage:HashMap<(i32,i32),Vec<T>>,
-    min:(i32,i32),
-    max:(i32,i32)
+    pub storage:HashMap<(i32,i32),Vec<T>>,
+    pub min:(i32,i32),
+    pub max:(i32,i32)
 }
 
-impl<T> Bin2D<T> where T:Copy {
+impl<T> Bin2D<T> where T:Zero + Float {
     pub fn new<G>(x:&[G],y:&[G],vals:&[T],bin_size:f32) -> Self
     where G:Into<f32> + Copy
      {
@@ -85,9 +112,45 @@ impl<T> Bin2D<T> where T:Copy {
         }
     }
 
+    pub fn dimensions(&self) -> (usize,usize) {
+        (
+            (self.max.0 - self.min.0 + 1) as usize,
+            (self.max.1 - self.min.1 + 1) as usize
+        )
+    }
+
+    pub fn aggregate(&self,t:Statistic) -> Vec<T> {
+        let dims = self.dimensions();
+        let n = dims.0 * dims.1;
+        let mut out = vec![T::nan();n];
+        for (coord,thing) in &self.storage {
+            let sub_x = (coord.0 - self.min.0) as usize;
+            let sub_y = (coord.1 - self.min.1) as usize;
+            let idx = subscripts_to_index_col_major(sub_x,sub_y,dims.0,dims.1).unwrap();
+            match &t {
+                Statistic::Mean => out[idx] = mean(thing).unwrap_or(T::nan()),
+                Statistic::StandardDev => out[idx] = standard_dev(thing).unwrap_or(T::nan()),
+                Statistic::Variance => out[idx] = variance(thing).unwrap_or(T::nan()),
+                Statistic::Skewness => out[idx] = skewness(thing).unwrap_or(T::nan()),
+                Statistic::Kurtosis => out[idx] = kurtosis(thing).unwrap_or(T::nan()),
+                Statistic::Count => out[idx] = T::from(thing.len()).unwrap_or(T::nan()),
+                
+            }
+        }
+        out
+    }
+
 }
 
-
+#[derive(Copy,Clone,Debug)]
+pub enum Statistic {
+    Mean,
+    Variance,
+    StandardDev,
+    Skewness,
+    Kurtosis,
+    Count,
+}
 
 fn mean<T:Zero + Copy + Num + Real>(x:&[T]) -> Option<T> {
 
@@ -99,82 +162,47 @@ fn mean<T:Zero + Copy + Num + Real>(x:&[T]) -> Option<T> {
     for t in x {
         sum = sum + *t;
     }
-    sum = sum / x.len().into();
+
+    sum = sum / T::from(x.len())?;
 
     Some(sum)
 }
 
 fn variance<T:Zero + Copy + Num + Real>(x:&[T]) -> Option<T> { 
-
-    if x.len() <= 1 {
-        return None
-    }
-
-    let mean = mean(x).unwrap();
-
-    let mut std = T::zero();
-    for t in x {
-        let d = *t - mean;
-        std = std + d*d;
-    }
-
-    std = std / (x.len() - 1).into();
-
-    Some(std)
+    central_moment(x, 2)
 }
 
-fn standard_dev<T:Zero + Copy + Num + From<usize> + Real>(x:&[T]) -> Option<T> { 
+fn standard_dev<T:Zero + Copy + Num + Real>(x:&[T]) -> Option<T> { 
     Some(variance(x)?.sqrt())
 }
 
-fn skewness<T:Zero + Copy + Num + From<usize> + Real>(x:&[T]) -> Option<T> {
-    let mean = mean(x)?;
-    let std = standard_dev(x)?;
-    let mut skew = T::zero();
-    for t in x {
-        let d = *t - mean;
-        skew = skew + d.powi(3);
-    }
-    skew = skew / <T as From<usize>>::from(x.len()-1) * std.powi(3);
-    Some(skew)
+fn skewness<T:Zero + Copy + Num + Real>(x:&[T]) -> Option<T> {
+    Some(central_moment(x, 3)? / standard_dev(x)?.powi(3))
 }
 
-fn kurtosis<T:Zero + Copy + Num + From<usize> + Real>(x:&[T]) -> Option<T> {
-    let mean = mean(x)?;
-    let std = standard_dev(x)?;
-    let mut kurt = T::zero();
-    for t in x {
-        let d = *t - mean;
-        kurt = kurt + d.powi(4);
-    }
-    kurt = kurt / <T as From<usize>>::from(x.len()-1) * std.powi(4);
-    Some(kurt)
+fn kurtosis<T:Zero + Copy + Num + Real>(x:&[T]) -> Option<T> {
+    Some(central_moment(x, 4)? / standard_dev(x)?.powi(4))
 }
 
-pub enum Statistic {
-    Mean,
-    Variance,
-    Skewness,
-    Kurtosis,
+fn central_moment<T:Zero + Copy + Num + Real>(x:&[T],order:i32) -> Option<T> {
+    let mean = mean(x)?;
+    let mut moment = T::zero();
+    for sample in x {
+        let resid = *sample - mean;
+        moment = moment + resid.powi(order);
+    }
+    moment = moment /  T::from(x.len())?;
+    Some(moment)
 }
 
 fn bin(x:f32,bin_size:f32) -> i32 {
     (x / bin_size) as i32
 }
 
-
-fn coord_to_sub(x:f64,y:f64,i_len:f64,j_len:f64) -> (usize,usize) {
-
-    todo!()
-
-}
-
-
-
 // Function to convert matrix subscripts to a linear index in column-major order
 fn subscripts_to_index_col_major(row: usize, col: usize, num_rows: usize, num_cols: usize) -> Result<usize,Box<dyn Error>> {
     if !(row < num_rows && col < num_cols) {
-        Err("invalid subscripts")?
+        Err(format!("{},{},{},{}",row,num_rows,col,num_cols))?
     }
     // Calculate the linear index
     Ok(col * num_rows + row)
